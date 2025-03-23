76b0ac3c2e8d530aea7cd3741e3c79065afc858f
nikita <NikitaPuzyrenko93@gmail.com>
2025-03-23 13:01:33 +0200

    chore(tailwind,pinia): add dependency and config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  compatibilityDate: '2025-03-23',
+  devtools: {enabled: true},
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+
+  typescript: {
+      strict: true
+  },
+
 })
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+    content: [
+        './app.vue',
+        './pages/**/*.vue',
+        './components/**/*.vue',
+        './features/**/*.vue',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
